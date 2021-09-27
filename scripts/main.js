eead122151f463faba42d0090a5f0f900285503d
Shubham Gupta <shubhastro2@gmail.com>
2021-09-28 00:37:37 +0530

    Navbar and sidebar completed for the computer version.

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+function sidebar_toggle(type){
+    let sidebar = document.querySelector('.sidebar');
+    let main = document.querySelector('.main');
+
+    if(sidebar.classList.contains('sidebar-open')){
+        sidebar.classList.remove('sidebar-open');
+        main.classList.remove('complete-open');
+        if(type === 'partial'){
+            sidebar.classList.add('sidebar-partial');
+            main.classList.add('partial-open');
+        }
+    }
+
+    else{
+        sidebar.classList.add('sidebar-open');
+        main.classList.add('complete-open');
+        if(type === 'partial'){
+            sidebar.classList.remove('sidebar-partial');
+            main.classList.remove('partial-open');
+        }
+    }
+}
+
+window.addEventListener('DOMContentLoaded', (event)=>{
+    let sidebar = document.querySelector('.sidebar');
+    let main = document.querySelector('.main');
+
+    if(sidebar.classList.contains('sidebar-partial')){
+        main.classList.add('partial-open');
+    }
+
+    else if(sidebar.classList.contains('sidebar-open')){
+        main.classList.add('complete-open');
+    }
+})
