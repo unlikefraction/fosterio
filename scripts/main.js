d90def9f2bccaeb02a9fb6b8a3966731b7eb5f65
Shubham Gupta <shubhastro2@gmail.com>
2021-10-06 18:27:56 +0530

    footer basic completed

--- scripts/main.js
+++ scripts/main.js
@@ -5,22 +5,41 @@ let tabletBreakpoint = 1024;
 function sidebar_toggle(type){
     let sidebar = document.querySelector('.sidebar');
     let main = document.querySelector('.main');
+    let footer = document.querySelector('footer');
 
     if(sidebar.classList.contains('sidebar-open')){
         sidebar.classList.remove('sidebar-open');
         main.classList.remove('complete-open');
+
+        if(footer){
+            footer.classList.remove('complete-open');
+        }
+
         if(type === 'partial'){
             sidebar.classList.add('sidebar-partial');
             main.classList.add('partial-open');
+
+            if(footer){
+                footer.classList.add('partial-open');
+            }
         }
     }
 
     else{
         sidebar.classList.add('sidebar-open');
         main.classList.add('complete-open');
+
+        if(footer){
+            footer.classList.add('complete-open');
+        }
+
         if(type === 'partial'){
             sidebar.classList.remove('sidebar-partial');
             main.classList.remove('partial-open');
+
+            if(footer){
+                footer.classList.remove('partial-open');
+            }
         }
     }
 }
@@ -30,14 +49,21 @@ window.addEventListener('DOMContentLoaded', (event)=>{
     // Sidebar status to Main body
     let sidebar = document.querySelector('.sidebar');
     let main = document.querySelector('.main');
+    let footer = document.querySelector('footer');
 
     if(sidebar){
         if(sidebar.classList.contains('sidebar-partial')){
             main.classList.add('partial-open');
+            if(footer){
+                footer.classList.add('partial-open');
+            }
         }
     
         else if(sidebar.classList.contains('sidebar-open')){
             main.classList.add('complete-open');
+            if(footer){
+                footer.classList.add('complete-open');
+            }
         }
     }
     // Sidebar status instantiation ends
