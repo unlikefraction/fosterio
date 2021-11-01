b8d0d915a89dc87509f80942f1ecb6fa88964e46
Shubham Gupta <shubhastro2@gmail.com>
2021-11-01 15:59:13 +0530

    Glow Buttons and Cards

--- scripts/developer.js
+++ scripts/developer.js
@@ -98,7 +98,6 @@ function parseCode(code){
         return {
             '<': '&lt;',
             '>': '&gt;',
-            '&': '&amp;',
             "'": '\'',
             '"': '\"',
             '`': '\`',
